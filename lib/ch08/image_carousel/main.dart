d61f589e768c6b3866592006cc1b6f33d50362ae
johyonghoon <johyonghoon01@gmail.com>
2023-02-05 17:31:59 +0900

    image carousel

---
+++ lib/ch08/image_carousel/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ios/ch08/image_carousel/screens/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: HomeScreen(),
+      ),
+    ),
+  );
+}
