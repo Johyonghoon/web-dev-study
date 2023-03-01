2347b1d9a95a4b5d47bad630cc2be0ff6ec7e37e
johyonghoon <johyonghoon01@gmail.com>
2023-03-01 14:48:44 +0900

    navigation exrc ing...

---
+++ lib/ch10/button/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ios/ch10/button/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
