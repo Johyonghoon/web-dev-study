192c132dee0d45e00a0fd7ce263676cf9690c463
johyonghoon <johyonghoon01@gmail.com>
2023-03-13 14:59:02 +0900

    image upload and send to fastapi done

--- lib/ch10/navigation/screen/route_one_screen.dart
+++ lib/ch10/navigation/screen/route_one_screen.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ios/ch10/navigation/layout/main_layout.dart';
+import 'package:flutter_ios/ch10/navigation/screen/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
-  const RouteOneScreen({Key? key}) : super(key: key);
+  final int number;
+
+  const RouteOneScreen({
+    required this.number,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Route One"),
-      ),
+    return MainLayout(
+      title: "Route One",
+      children: [
+        Text(
+          number.toString(),
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop(456);
+          },
+          child: Text('Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => RouteTwoScreen(),
+                settings: RouteSettings(
+                  arguments: 789,
+                )));
+          },
+          child: Text('Push'),
+        )
+      ],
     );
   }
 }
